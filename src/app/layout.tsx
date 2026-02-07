c8b004e7b94fb8cac8ef7051c97cea09a38c7d08
Ash Nayak <your-email@example.com>
2026-02-07 16:02:52 +0530

    notification implemented & some fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { ClientProviders } from '@/components/ClientProviders';
 
@@ -15,6 +16,20 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className="antialiased">
+                <Script
+                    src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js"
+                    defer
+                />
+                <Script id="onesignal-init">
+                    {`
+                      window.OneSignalDeferred = window.OneSignalDeferred || [];
+                      OneSignalDeferred.push(async function(OneSignal) {
+                        await OneSignal.init({
+                          appId: "30756cd3-0f13-41cc-ad4c-6ff64f1c4d9c",
+                        });
+                      });
+                    `}
+                </Script>
                 <ClientProviders>
                     {children}
                 </ClientProviders>
